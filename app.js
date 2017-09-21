f97f7f3cf5771b57f281ebf4217c29af04eeecb7
Soaresb <soaresb@bu.edu>
2017-09-21 03:02:11 -0400

    please work

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ app.use(bodyParser.json());
 
 app.use('/routes',routes);
 
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
 app.listen(port, () => {
   console.log("server started on port " +port);
 });
